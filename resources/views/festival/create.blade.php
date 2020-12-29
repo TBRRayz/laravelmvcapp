c00d2579d876fb685d5be48189f8e84d25ddca4e
bryan <bryan@routigo.com>
2020-12-29 15:57:03 +0100

    add the input fields to the create view

--- resources/views/festival/create.blade.php
+++ resources/views/festival/create.blade.php
@@ -2,40 +2,94 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-3 p-5">
-            <img src="/svg/defqon_logo.svg" class="rounded-circle w-100">
-        </div>
-        <div class="col-9 p-5">
-            <div><h1>{{ $user->username }}</h1></div>
-            <div class="d-flex">
-                <div class="pr-5"><strong>13</strong> Likes</div>
-                <div class="pr-5"><strong>124</strong> comments</div>
-            </div>
-            <div class="pt-4 font-weight-bold"> {{ $user->userProfile->title }}</div>
-            <div>{{ $user->userProfile->description }}</div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-2 p-5">
-            <img src="/svg/defqon_logo.svg" class="rounded-circle w-100">
-        </div>
-        <div class="col-2 p-5">
-            <img src="/svg/decibel.svg" class="rounded-circle w-100">
-        </div>
-        <div class="col-2 p-5">
-            <img src="/svg/qbase.svg" class="rounded-circle w-100">
-        </div>
-        <div class="col-2 p-5">
-            <img src="/svg/decibel.svg" class="rounded-circle w-100">
-        </div>
-        <div class="col-2 p-5">
-            <img src="/svg/qbase.svg" class="rounded-circle w-100">
-        </div>
-        <div class="col-2 p-5">
-            <img src="/svg/defqon_logo.svg" class="rounded-circle w-100">
-        </div>
+    <form action="/festival" enctype="multipart/form-data" method="post">
+        @csrf
 
-    </div>
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="row col-md-6 offset-md-4">
+                    <h1>Add New Festival</h1>
+                </div>
+                <div class="form-group row pt-4">
+                    <label for="festivalName" class="col-md-4 col-form-label text-md-right">Name</label>
+
+                    <div class="col-md-6">
+                        <input id="festivalName"
+                               type="text"
+                               class="form-control @error('festivalName') is-invalid @enderror"
+                               name="festivalName"
+                               value="{{ old('festivalName') }}"
+                               required
+                               autocomplete="festivalName" autofocus>
+
+                        @error('festivalName')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="title" class="col-md-4 col-form-label text-md-right">Tile</label>
+
+                    <div class="col-md-6">
+                        <input id="title"
+                               type="text"
+                               class="form-control @error('title') is-invalid @enderror"
+                               name="title"
+                               value="{{ old('title') }}"
+                               required
+                               autocomplete="title" autofocus>
+
+                        @error('title')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
+
+                    <div class="col-md-6">
+                        <input id="description"
+                               type="text"
+                               class="form-control @error('description') is-invalid @enderror"
+                               name="description"
+                               value="{{ old('description') }}"
+                               required
+                               autocomplete="description" autofocus>
+
+                        @error('description')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row">
+                    <label for="festivalImage" class="col-md-4 col-form-label text-md-right">Image</label>
+
+                    <div class="col-md-6">
+                        <input type="file" class="form-control-file" id="festivalImage" name="festivalImage">
+
+                        @error('festivalImage')
+                            <strong>{{ $message }}</strong>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row pt-2">
+                    <div class="col-md-6 offset-md-4">
+                        <button class="btn btn-primary">Add Festival</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </form>
 </div>
 @endsection
