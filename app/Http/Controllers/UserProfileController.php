c0389aec7eb45bf0d59d98169554118dc59fb431
bryan <bryan@routigo.com>
2021-01-03 16:17:08 +0100

    add the functions to update a user profile

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -2,16 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Festival;
 use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Http\Request;
 
 class UserProfileController extends Controller
 {
-    public function index($user)
+    public function index(User $user)
     {
-        $user = User::findOrFail($user);
+
         return view('user.user', [
             'user' => $user,
         ]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', [UserProfile::class, $user->userProfile]);
+
+        return view('user.edit', [
+            'user' => $user,
+        ]);
+    }
+
+    public function update(User $user) {
+
+        $this->authorize('update', [UserProfile::class, $user->userProfile]);
+
+        $data = request()->validate([
+            'title' => '',
+            'description' => '',
+            'url' => 'url',
+            'Image' => 'image',
+        ]);
+
+        if (request('Image')) {
+            $imagePath = request('Image')->store('userProfile', 'public');
+
+        }
+
+        $user->userProfile()->update(array_merge(
+            $data,
+            ['Image' => $imagePath]
+        ));
+
+        return redirect("/user/{$user->id}");
+
     }
 }
