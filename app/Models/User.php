26c0550e70064a9fcadbfd0917e1e616d87395ad
bryan <bryan@routigo.com>
2021-01-06 21:15:23 +0100

    add the relations in the models

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Auth;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -53,6 +54,11 @@ class User extends Authenticatable
                 'title' => $user->name,
             ]);
         });
+    }
+
+    public function checkIn(): BelongsToMany
+    {
+        return $this->belongsToMany(Festival::class);
     }
 
     public function userProfile(): HasOne
