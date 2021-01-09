55dd90302c25cb2fa239ec2be102d86172a6faa7
bryan <bryan@routigo.com>
2021-01-09 16:35:52 +0100

    add the search view

---
+++ resources/views/search.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+        @foreach($festivals as $festival)
+        <div class="row">
+            <div class="col-3 p-5">
+                <a href="/festival/{{$festival->id}}">
+                    <img src="/storage/{{ $festival->festivalImage }}" class="rounded-circle w-100">
+                </a>
+            </div>
+            <div class="col-9 p-5">
+                <div><h1>{{ $festival->festivalName }}</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>13</strong> Likes</div>
+                    <div class="pr-5"><strong>124</strong> comments</div>
+                </div>
+                <div class="pt-4 font-weight-bold">{{ $festival->title }}</div>
+            </div>
+
+        </div>
+        @endforeach
+</div>
+@endsection
