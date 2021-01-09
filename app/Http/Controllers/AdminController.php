035fafcbe4ec875c214e8371d807669de9c666cd
bryan <bryan@routigo.com>
2021-01-09 21:41:13 +0100

    Add a tabel for the admin with the users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Festival;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function indexFestival()
     {
         $festivalData = Festival::all();
         return view('admin.festival', ['festivals'=>$festivalData]);
+    }
+
+    public function indexUser()
+    {
+        $userData = User::all();
+        return view('admin.user', ['users'=>$userData]);
     }
 }
